c785544453c2e4c5adc0de17a335f8ac24b63a83
MarcosEstPam <marcosepamias@gmail.com>
2024-06-24 18:41:05 +0000

    terminado

---
+++ app.js
@@ -0,0 +1,11 @@
+document.addEventListener ("DOMContentLoaded", (event) =>{
+    const botonmegusta = document.querySelectorAll(".iconomegusta i");
+    botonmegusta.forEach(button => {
+        button.addEventListener("click",()=>{
+            button.classList.toggle("fa-regular");
+            button.classList.toggle("fa-solid");
+            button.lassList.toggle("iconomegusta-activo");
+        }
+        
+    });
+}
