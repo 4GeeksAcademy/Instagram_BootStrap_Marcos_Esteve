162b34dca4c0ef666aa43b8e8c5147db1f328be0
MarcosEstPam <marcosepamias@gmail.com>
2024-06-24 19:08:30 +0000

    terminado

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    const botonmegusta = document.querySelectorAll(".iconomegusta i");
-    botonmegusta.forEach(button => {
-        button.addEventListener("click", () => {
-            button.classList.toggle("fa-regular");
-            button.classList.toggle("fa-solid");
-            button.classList.toggle("iconomegusta-activo");
-        });
+document.addEventListener("DOMContentLoaded", () => {
+    const likeIcon = document.querySelector(".iconomegusta");
+    const likesCount = document.getElementById("likes");
+
+    // Generar número aleatorio entre 0 y 1000
+    let Likesiniciales = Math.floor(Math.random() * 1001);
+    likesCount.textContent = `${Likesiniciales} likes`;
+
+    likeIcon.addEventListener("click", () => {
+        if (likeIcon.classList.contains("iconomegusta-activo")) {
+            Likesiniciales--;
+            likeIcon.classList.remove("fa-solid");
+            likeIcon.classList.add("fa-regular");
+        } else {
+            Likesiniciales++;
+            likeIcon.classList.remove("fa-regular");
+            likeIcon.classList.add("fa-solid");
+        }
+        likesCount.textContent = `${Likesiniciales} likes`;
+        likeIcon.classList.toggle("iconomegusta-activo");
     });
 });
